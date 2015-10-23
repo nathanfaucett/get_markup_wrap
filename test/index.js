1fd4bde2fed5a700eebe7faccee9af7ce44655ff
nathanfaucett <nathanfaucett@gmail.com>
2015-10-22 22:46:59 -0500

    updates

--- test/index.js
+++ test/index.js
@@ -1,9 +1,12 @@
 var tape = require("tape"),
+    environment = require("environment"),
     getMarkupWrap = require("..");
 
 
 tape("getMarkupWrap(nodeName : String) should return null if nodeName should not be wraped array helper if needed", function(assert) {
-    assert.deepEqual(getMarkupWrap("area"), null);
-    assert.deepEqual(getMarkupWrap("circle"), [1, "<svg>", "</svg>"]);
+    if (!environment.node) {
+        assert.deepEqual(getMarkupWrap("area"), null);
+        assert.deepEqual(getMarkupWrap("circle"), [1, "<svg>", "</svg>"]);
+    }
     assert.end();
 });
